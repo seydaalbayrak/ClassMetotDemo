1acc1ea7a90b44ba8c35a48759e9128f1ce175b4
Şeyda Albayrak <seydaalbayrak02@outlook.com>
2021-03-10 21:11:51 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Eklendi : " + musteri.Ad + " " + musteri.Soyad);
+        }
+
+        public void Listele(Musteri[] musteri)
+        {
+            foreach (var i in musteri)
+            {
+                Console.WriteLine(i.Ad + " " + i.Soyad);
+                
+            }
+            
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Silindi : " + musteri.Ad + " " + musteri.Soyad);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Şeyda";
+            musteri1.Soyad = "Albayrak";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Şule";
+            musteri2.Soyad = "Albayrak";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            Console.WriteLine("---------------------------");
+            musteriManager.Sil(musteri2);
+            Console.WriteLine("---------------------------");
+            musteriManager.Listele(musteriler);
+
+
+
+        }
+    }
+}
